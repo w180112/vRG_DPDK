76d120e639e4229c17d5901744b8ee0e3bd10d82
w180112 <w180112@gmail.com>
2019-07-08 16:23:03 +0800

    merge dpdk source code

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+export RTE_SDK=./src/lib/dpdk-stable-18.11.2/
+export RTE_TARGET=x86_64-native-linuxapp-gcc
 cd ./src
+cd ./lib/dpdk-stable-18.11.2 && make install T=x86_64-native-linuxapp-gcc
+cd ../..
 make
 cd ..
