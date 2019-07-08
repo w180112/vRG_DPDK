740bea34db65e4ac6b603bc19cd3036584814831
w180112 <w180112@gmail.com>
2019-07-08 19:53:45 +0800

    update ./install path

--- install.sh
+++ install.sh
@@ -1,7 +1,20 @@
-export RTE_SDK=./src/lib/dpdk-stable-18.11.2/
+get_script_dir () {
+     SOURCE="${BASH_SOURCE[0]}"
+     # While $SOURCE is a symlink, resolve it
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )" 
+          # If $SOURCE was a relative symlink (so no "/" as prefix, need to resolve it relative to the symlink base directory
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+     echo "$DIR"
+}
+
+export RTE_SDK=$(get_script_dir)/src/lib/dpdk-stable-18.11.2 
 export RTE_TARGET=x86_64-native-linuxapp-gcc
 cd ./src
-#cd ./lib/dpdk-stable-18.11.2 && make install T=x86_64-native-linuxapp-gcc
-#cd ../..
+cd ./lib/dpdk-stable-18.11.2 && make install T=x86_64-native-linuxapp-gcc
+cd ../.. 
 make
-cd ..
+cd .. 
